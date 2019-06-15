0548a8f09c2c897e82fad2b7873a4e53c1e35b2c
zvecr <git@zvecr.com>
2019-06-15 18:47:11 +0100

    Initial stash

---
+++ lib/components/RouterLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="$router.send(event)"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: 'a'
+    },
+    event:{
+      type: String,
+      default: 'navigate' 
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ lib/components/RouterView.vue
@@ -0,0 +1,13 @@
+<template>
+    <component :is="$route"></component>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
